209bf6d5af73d89c6630d3792cfa1008d1f400bd
Toke Høiland-Jørgensen <toke@toke.dk>
2017-03-31 23:05:14 +0200

    Client and server can exchange encrypted secrets

---
+++ nsregc.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+//	"net"
+	"os"
+//	"os/signal"
+	"runtime/pprof"
+	"strconv"
+//	"strings"
+//	"syscall"
+//	"time"
+	"math/big"
+
+//	"keydb"
+
+//	"encoding/binary"
+	"encoding/base64"
+	"crypto/rsa"
+	"crypto/rand"
+	"crypto/sha256"
+
+//	"crypto"
+	"github.com/miekg/dns"
+)
+
+const dom = "nsregd.example.org."
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	printf     = flag.Bool("print", false, "print replies")
+	compress   = flag.Bool("compress", false, "compress replies")
+	tsig       = flag.String("tsig", "", "use MD5 hmac tsig: keyname:base64")
+	hostname   = flag.String("host", "localhost", "DNS server hostname")
+	secret     = flag.String("secret", "", "Secret")
+	port       = flag.Int("port", 53, "Port number")
+)
+
+func fromBase64(s []byte) (buf []byte, err error) {
+	buflen := base64.StdEncoding.DecodedLen(len(s))
+	buf = make([]byte, buflen)
+	n, err := base64.StdEncoding.Decode(buf, s)
+	buf = buf[:n]
+	return
+}
+
+
+func toKEY(dk *dns.DNSKEY) *dns.KEY {
+	k := &dns.KEY{DNSKEY: *dk}
+	k.Hdr = *new(dns.RR_Header)
+	k.Hdr.Name = dk.Hdr.Name
+	k.Hdr.Rrtype = dns.TypeKEY
+	k.Hdr.Class = dk.Hdr.Class
+	k.Hdr.Ttl = dk.Hdr.Ttl
+	k.Hdr.Rdlength = dk.Hdr.Rdlength
+	return k
+}
+
+func publicKeyRSA(k *dns.DNSKEY) *rsa.PublicKey {
+	keybuf, err := fromBase64([]byte(k.PublicKey))
+	if err != nil {
+		return nil
+	}
+
+	// RFC 2537/3110, section 2. RSA Public KEY Resource Records
+	// Length is in the 0th byte, unless its zero, then it
+	// it in bytes 1 and 2 and its a 16 bit number
+	explen := uint16(keybuf[0])
+	keyoff := 1
+	if explen == 0 {
+		explen = uint16(keybuf[1])<<8 | uint16(keybuf[2])
+		keyoff = 3
+	}
+	pubkey := new(rsa.PublicKey)
+
+	pubkey.N = big.NewInt(0)
+	shift := uint64((explen - 1) * 8)
+	expo := uint64(0)
+	for i := int(explen - 1); i > 0; i-- {
+		expo += uint64(keybuf[keyoff+i]) << shift
+		shift -= 8
+	}
+	// Remainder
+	expo += uint64(keybuf[keyoff])
+	if expo > 2<<31 {
+		// Larger expo than supported.
+		// println("dns: F5 primes (or larger) are not supported")
+		return nil
+	}
+	pubkey.E = int(expo)
+
+	pubkey.N.SetBytes(keybuf[keyoff+int(explen):])
+	return pubkey
+}
+
+
+
+
+func main() {
+//	var name, secret string
+	var err error
+	flag.Usage = func() {
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	domain := flag.Arg(0)
+/*	if *tsig != "" {
+		a := strings.SplitN(*tsig, ":", 2)
+		name, secret = dns.Fqdn(a[0]), a[1] // fqdn the name, which everybody forgets...
+	}*/
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	nameserver := dns.Fqdn(*hostname) + ":" + strconv.Itoa(*port)
+
+	c := new(dns.Client)
+	c.Net = "tcp"
+	
+	o := new(dns.OPT)
+	o.Hdr.Name = "."
+	o.Hdr.Rrtype = dns.TypeOPT
+	o.SetUDPSize(dns.DefaultMsgSize)
+
+	m := &dns.Msg{
+	MsgHdr: dns.MsgHdr{
+		RecursionDesired: false,
+		Opcode: dns.OpcodeQuery,
+	},
+		Question: make([]dns.Question, 1),
+	}
+
+	m.Question[0] = dns.Question{Name: dns.Fqdn(domain),
+		Qtype: dns.TypeKEY,
+		Qclass: uint16(dns.ClassINET)}
+
+	r, _, err := c.Exchange(m, nameserver)
+
+
+	switch err {
+	case nil:
+		fmt.Println(r.String())
+	default:
+		fmt.Println(err.Error())
+		return
+	}
+
+	rr := r.Answer[0]
+	if t, ok := rr.(*dns.KEY); ok {
+		pk := publicKeyRSA(&t.DNSKEY)
+		encsec, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pk,
+			[]byte(*secret), nil)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		m = &dns.Msg{
+			MsgHdr: dns.MsgHdr{
+				RecursionDesired: false,
+				Opcode: dns.OpcodeQuery,
+			},
+			Question: make([]dns.Question, 1),
+		}
+
+		fmt.Println(encsec)
+
+		m.Question[0] = dns.Question{Name: dns.Fqdn(domain),
+			Qtype: dns.TypeTKEY,
+			Qclass: uint16(dns.ClassINET)}
+
+		b64 := base64.StdEncoding.EncodeToString(encsec)
+		m.Extra = append(m.Extra, &dns.TKEY{
+			Hdr: dns.RR_Header{Name: dom, Rrtype: dns.TypeTKEY,
+				Class: dns.ClassINET, Ttl: 0},
+			Algorithm: dns.Fqdn(domain),
+			Mode: 4,
+			KeySize: uint16(len(b64)),
+			Key: b64,
+		})
+		m.Extra = append(m.Extra, rr)
+		m.Extra = append(m.Extra, o)
+
+		fmt.Printf("%d: %s\n", m.Len(), m.String())
+
+		r, _, err := c.Exchange(m, nameserver)
+		switch err {
+		case nil:
+			fmt.Println(r.String())
+		default:
+			fmt.Println(err.Error())
+			return
+		}
+	}
+
+}
--- nsregd.go
+++ nsregd.go
@@ -4,14 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
+//	"net"
 	"os"
 	"os/signal"
 	"runtime/pprof"
-	"strconv"
+//	"strconv"
 	"strings"
 	"syscall"
-	"time"
+//	"time"
 
 //	"keydb"
 
@@ -19,7 +19,7 @@ import (
 	"encoding/base64"
 	"crypto/rsa"
 	"crypto/rand"
-//	"crypto/sha256"
+	"crypto/sha256"
 
 	"crypto"
 	"github.com/miekg/dns"
@@ -75,22 +75,22 @@ func handleKeyReg(w dns.ResponseWriter, r *dns.Msg, m *dns.Msg) {
 		return
 	}
 
-	opts := rsa.OAEPOptions{Hash: crypto.SHA256}
-	pk := privkey.(rsa.PrivateKey)
+	pk := privkey.(*rsa.PrivateKey)
 	encbuf, err := fromBase64([]byte(tkey.Key))
 	if err != nil {
-		fmt.Println("Base64 decode error: %s", err.Error())
+		fmt.Println("Base64 decode error:", err.Error())
 		m.Rcode = dns.RcodeFormatError
 		return
 	}
-	deckey, err := pk.Decrypt(rand.Reader, encbuf, opts)
+	fmt.Println(encbuf)
+	deckey, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, pk, encbuf, nil)
 	if err != nil {
-		fmt.Println("Decrypt error: %s", err.Error())
+		fmt.Println("Decrypt error:", err.Error())
 		m.Rcode = dns.RcodeFormatError
 		return
 	}
 
-	fmt.Println("Got decrypted key: %s", base64.StdEncoding.EncodeToString(deckey))
+	fmt.Println("Got decrypted key:", string(deckey))
 
 }
 
@@ -114,12 +114,15 @@ func handleRegd(w dns.ResponseWriter, r *dns.Msg) {
 		rr dns.RR
 		addr net.IP
 	)*/
+
+	fmt.Println(r.String())
 	m := new(dns.Msg)
 	m.SetReply(r)
 
 
 	switch r.Question[0].Qtype {
 	case dns.TypeTKEY:
+		log.Println("Got TKEY request")
 		handleKeyReg(w, r, m)
 	case dns.TypeKEY:
 		log.Println("Got KEY request")
@@ -128,6 +131,9 @@ func handleRegd(w dns.ResponseWriter, r *dns.Msg) {
 		} else {
 			m.Rcode = dns.RcodeNameError
 		}
+	default:
+		log.Println("Got unknown type")
+		m.Rcode = dns.RcodeNotImplemented
 	}
 
 	if *printf {
@@ -137,88 +143,6 @@ func handleRegd(w dns.ResponseWriter, r *dns.Msg) {
 	if err != nil {
 		fmt.Printf("error on write: %s\n", err.Error())
 	}
-}
-
-func handleReflect(w dns.ResponseWriter, r *dns.Msg) {
-	var (
-		v4  bool
-		rr  dns.RR
-		str string
-		a   net.IP
-	)
-	m := new(dns.Msg)
-	m.SetReply(r)
-	m.Compress = *compress
-	if ip, ok := w.RemoteAddr().(*net.UDPAddr); ok {
-		str = "Port: " + strconv.Itoa(ip.Port) + " (udp)"
-		a = ip.IP
-		v4 = a.To4() != nil
-	}
-	if ip, ok := w.RemoteAddr().(*net.TCPAddr); ok {
-		str = "Port: " + strconv.Itoa(ip.Port) + " (tcp)"
-		a = ip.IP
-		v4 = a.To4() != nil
-	}
-
-	if v4 {
-		rr = &dns.A{
-			Hdr: dns.RR_Header{Name: dom, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
-			A:   a.To4(),
-		}
-	} else {
-		rr = &dns.AAAA{
-			Hdr:  dns.RR_Header{Name: dom, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0},
-			AAAA: a,
-		}
-	}
-
-	t := &dns.TXT{
-		Hdr: dns.RR_Header{Name: dom, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0},
-		Txt: []string{str},
-	}
-
-	switch r.Question[0].Qtype {
-	case dns.TypeTXT:
-		m.Answer = append(m.Answer, t)
-		m.Extra = append(m.Extra, rr)
-	default:
-		fallthrough
-	case dns.TypeAAAA, dns.TypeA:
-		m.Answer = append(m.Answer, rr)
-		m.Extra = append(m.Extra, t)
-	case dns.TypeAXFR, dns.TypeIXFR:
-		c := make(chan *dns.Envelope)
-		tr := new(dns.Transfer)
-		defer close(c)
-		if err := tr.Out(w, r, c); err != nil {
-			return
-		}
-		soa, _ := dns.NewRR(`whoami.miek.nl. 0 IN SOA linode.atoom.net. miek.miek.nl. 2009032802 21600 7200 604800 3600`)
-		c <- &dns.Envelope{RR: []dns.RR{soa, t, rr, soa}}
-		w.Hijack()
-		// w.Close() // Client closes connection
-		return
-	}
-
-	if r.IsTsig() != nil {
-		if w.TsigStatus() == nil {
-			m.SetTsig(r.Extra[len(r.Extra)-1].(*dns.TSIG).Hdr.Name, dns.HmacMD5, 300, time.Now().Unix())
-		} else {
-			println("Status", w.TsigStatus().Error())
-		}
-	}
-	if *printf {
-		fmt.Printf("%v\n", m.String())
-	}
-	// set TC when question is tc.miek.nl.
-	if m.Question[0].Name == "tc.miek.nl." {
-		m.Truncated = true
-		// send half a message
-		buf, _ := m.Pack()
-		w.Write(buf[:len(buf)/2])
-		return
-	}
-	w.WriteMsg(m)
 }
 
 func serve(net, name, secret string) {
@@ -256,7 +180,7 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	privkey, err = keyrr.Generate(2048)
+	privkey, err = keyrr.Generate(1024)
 	if err != nil {
 		log.Fatal(err)
 	}
